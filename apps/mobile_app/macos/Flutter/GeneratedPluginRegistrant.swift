cc94773ec35749393b58ec7e59e9b82b721c5f64
hasanat-ahmad <sahibzadahasanatahmad@gmail.com>
2026-08-28 11:35:14 +0500

    enhance: Update plugin registration to use RecordMacOsPlugin for macOS compatibility

--- apps/mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_picker_darwin
-import record_darwin
+import record_macos
 import shared_preferences_foundation
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
-  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
 }
